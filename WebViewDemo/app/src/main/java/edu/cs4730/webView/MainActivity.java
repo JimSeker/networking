3b4eceab5d576cb05338e8a942895529b094bf00
JimSeker <seker@uwyo.edu>
2015-05-07 09:12:16 -0600

    updated to API 22 and AppCompatActivity

--- WebViewDemo/app/src/main/java/edu/cs4730/webView/MainActivity.java
+++ WebViewDemo/app/src/main/java/edu/cs4730/webView/MainActivity.java
@@ -1,13 +1,12 @@
 package edu.cs4730.webView;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
 	MainFragment myMainFragment = null;
 	
