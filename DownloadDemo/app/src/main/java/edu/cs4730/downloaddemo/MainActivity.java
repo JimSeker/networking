61c2ebeb0b448161cdeebb508028134a3b9aeeb0
JimSeker <seker@uwyo.edu>
2016-10-27 13:07:36 -0600

    added permission checking and fix deprecated column.

--- DownloadDemo/app/src/main/java/edu/cs4730/downloaddemo/MainActivity.java
+++ DownloadDemo/app/src/main/java/edu/cs4730/downloaddemo/MainActivity.java
@@ -2,6 +2,8 @@ package edu.cs4730.downloaddemo;
 
 import java.io.FileNotFoundException;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -12,7 +14,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.database.Cursor;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -37,6 +41,10 @@ public class MainActivity extends AppCompatActivity {
     //SharedPreferences preferenceManager;
     DownloadManager downloadManager;
 
+    String TAG = "MainActivity";
+    public static final int REQUEST_PERM_ACCESS_noti = 1;
+    public static final int REQUEST_PERM_ACCESS_nonoti = 2;
+
     /**
      * Called when the activity is first created.
      */
@@ -52,17 +60,16 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View arg0) {
-                //setup download and how it will look on the notification bar.
-                Uri Download_Uri = Uri.parse(Download_path);
-                DownloadManager.Request request = new DownloadManager.Request(Download_Uri)
-                        .setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI | DownloadManager.Request.NETWORK_MOBILE)
-                        .setAllowedOverRoaming(false)
-                        .setTitle("file nasa")
-                        .setDescription("stuff ok.")
-                        .setShowRunningNotification(true)
-                        .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "nasapic.jpg");
-                download_id = downloadManager.enqueue(request);
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    //I'm on not explaining why, just asking for permission.
+                    Log.v(TAG, "asking for permissions");
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            MainActivity.REQUEST_PERM_ACCESS_noti);
 
+                } else {
+                    Log.v(TAG, "already have permissions");
+                    downloadfile();
+                }
             }
         });
         Button btnDownload2 = (Button) findViewById(R.id.download2);
@@ -70,19 +77,45 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View arg0) {
-
-                Uri Download_Uri = Uri.parse(Download_path);
-                DownloadManager.Request request = new DownloadManager.Request(Download_Uri)
-                        //.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI | DownloadManager.Request.NETWORK_MOBILE);
-                        //.setAllowedOverRoaming(true)
-                        .setNotificationVisibility(DownloadManager.Request.VISIBILITY_HIDDEN)  //api 11 and above!
-                        //.setShowRunningNotification(true)   //api 10 and below.  Doesn't work on 4.1.1 must use above.
-                        .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "nasapic.jpg");
-                download_id = downloadManager.enqueue(request);
-
+                downloadfilenonoti();
             }
         });
     }
+
+    void downloadfile() {
+        //setup download and how it will look on the notification bar.
+        Uri Download_Uri = Uri.parse(Download_path);
+        DownloadManager.Request request = new DownloadManager.Request(Download_Uri)
+                .setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI | DownloadManager.Request.NETWORK_MOBILE)
+                .setAllowedOverRoaming(false)
+                .setTitle("file nasa")
+                .setDescription("stuff ok.")
+                .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE)  //api 11 and above!
+                .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "nasapic.jpg");
+        download_id = downloadManager.enqueue(request);
+    }
+
+    void downloadfilenonoti() {
+
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            //I'm on not explaining why, just asking for permission.
+            Log.v(TAG, "asking for permissions");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    MainActivity.REQUEST_PERM_ACCESS_nonoti);
+            return;
+        }
+        Log.v(TAG, "already have permissions");
+
+        //This should down the file without creating a notification.
+        Uri Download_Uri = Uri.parse(Download_path);
+        DownloadManager.Request request = new DownloadManager.Request(Download_Uri)
+                //.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI | DownloadManager.Request.NETWORK_MOBILE);
+                //.setAllowedOverRoaming(true)
+                .setNotificationVisibility(DownloadManager.Request.VISIBILITY_HIDDEN)  //api 11 and above!
+                .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "nasapic.jpg");
+        download_id = downloadManager.enqueue(request);
+    }
+
 
     @Override
     protected void onResume() {
@@ -122,10 +155,10 @@ public class MainActivity extends AppCompatActivity {
                 int status = cursor.getInt(columnIndex);
                 int columnReason = cursor.getColumnIndex(DownloadManager.COLUMN_REASON);
                 int reason = cursor.getInt(columnReason);
-                int columnFname = cursor.getColumnIndex(DownloadManager.COLUMN_LOCAL_FILENAME);
+                int columnURI = cursor.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI);
                 String Fname;
-                if (columnFname >= 0) {
-                    Fname = cursor.getString(columnFname);
+                if (columnURI >= 0) {
+                    Fname = cursor.getString(columnURI);
                 } else {
                     Fname = "unknown";
                 }
@@ -166,6 +199,49 @@ public class MainActivity extends AppCompatActivity {
         }
 
     };
+
+
+    //handle the response.
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERM_ACCESS_noti: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // write permission was granted, yay!  Now kick off the download manager.
+                    downloadfile();
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Log.v(TAG, "write permissions not granted.");
+                }
+                return;
+            }
+            case REQUEST_PERM_ACCESS_nonoti: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // write permission was granted, yay!  Now kick off the download manager.
+                    downloadfilenonoti();
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Log.v(TAG, "write permissions not granted.");
+                }
+                return;
+            }
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
 
 
 }
