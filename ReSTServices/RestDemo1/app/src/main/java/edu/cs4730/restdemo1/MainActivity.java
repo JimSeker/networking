2962368f5350dbf518416fe369636d7f6d4ab260
Jim Ward <seker@uwyo.edu>
2022-03-22 10:07:59 -0600

    replaced the depreciated async task.

--- ReSTServices/RestDemo1/app/src/main/java/edu/cs4730/restdemo1/MainActivity.java
+++ ReSTServices/RestDemo1/app/src/main/java/edu/cs4730/restdemo1/MainActivity.java
@@ -1,7 +1,5 @@
 package edu.cs4730.restdemo1;
 
-import android.os.AsyncTask;
-import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -13,6 +11,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
@@ -25,6 +24,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -75,99 +75,88 @@ public class MainActivity extends AppCompatActivity {
         mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                //where we call the refresher parts.  normally some kind of networking async task or web service.
-                // For demo purpose, the code in the refreshslower method so it will take a couple of seconds
-                //otherise, the task or service would just be called here.
-                new doNetwork().execute(uri);
-                //refreshslower();  //this will be slower, for the demo.
+                new Thread(new doNetwork(uri)).start();
             }
         });
         mSwipeRefreshLayout.setRefreshing(true);
         //call the refresh code manually here.
-        new doNetwork().execute(uri);
+        new Thread(new doNetwork(uri)).start();
 
     }
 
-    /*
-     * Shows how to use an AsyncTask with a HttpClient method.
+    /**
+     * simple helper method to update the UI.
      */
-    private class doNetwork extends AsyncTask<URI, String, String> {
-
-        /*
-         * while this could have been in the doInBackground, I reused the
-         * method already created the thread class.
-         *
-         * This downloads a text file and returns it to doInBackground.
-         */
-        //Simple class that takes an InputStream and return the data
-        //as a string, with line separators (ie end of line markers)
-        private String readStream(InputStream in) {
-            BufferedReader reader = null;
-            StringBuilder sb = new StringBuilder("");
-            String line = "";
-            String NL = System.getProperty("line.separator");
-            //Log.wtf(TAG, "readstream started.");
-            try {
-                reader = new BufferedReader(new InputStreamReader(in));
-                while ((line = reader.readLine()) != null) {
-                    sb.append(line).append(NL);
+    public void mkmsg(String str) {
+        //so the client uses a handler, while the server code is going to use runUI method.
+        //the method name makes no sense here, but keeping the name, so it matches the tpcclient.
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    list = new JSONArray(str);
+                    mAdapter.setData(list);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getBaseContext(), "Data response is empty", Toast.LENGTH_SHORT).show();
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                mSwipeRefreshLayout.setRefreshing(false);  //turn off the refresh.
+            }
+        });
+    }
+
+
+    //Simple class that takes an InputStream and return the data
+    //as a string, with line separators (ie end of line markers)
+    private String readStream(InputStream in) {
+        BufferedReader reader = null;
+        StringBuilder sb = new StringBuilder("");
+        String line = "";
+        String NL = System.getProperty("line.separator");
+        try {
+            reader = new BufferedReader(new InputStreamReader(in));
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append(NL);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
-            return sb.toString();
+        }
+        return sb.toString();
+    }
+
+
+    /**
+     * actual thread class that calls executeHpptGet() above.
+     */
+    class doNetwork implements Runnable {
+        URI myUri;
+
+        doNetwork(URI uri) {
+            myUri = uri;
         }
 
-
-        @Override
-        protected String doInBackground(URI... params) {
+        public void run() {
             String page = "";
             try {
-                //URL url = new URL(params[0].toString()); //but next line is much better! convert directly.
-                URL url = params[0].toURL();
-                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                URL myUrl = uri.toURL();
+                HttpURLConnection con = (HttpURLConnection) myUrl.openConnection();
                 page = readStream(con.getInputStream());
                 con.disconnect();
             } catch (Exception e) {
-                // publishProgress("Failed to retrieve web page ...\n");
-                publishProgress(e.getMessage());
-
+                Log.wtf(TAG, e.getMessage());
+                return;
             }
-            return page;  //return the page downloaded.
-        }
-
-        /*
-         * This takes the place of handlers and my makemsg method, since we can directly access the screen.
-         */
-        protected void onProgressUpdate(String... progress) {
-            Log.wtf(TAG, progress[0]);
-        }
-
-        /*
-         * So the file has been downloaded and in this simple example it is displayed
-         * to the screen.
-         */
-        protected void onPostExecute(String result) {
             Log.wtf(TAG, "finished, updating result.");
-            // Log.wtf(TAG, "data: " + result);
-            try {
-                list = new JSONArray(result);
-                mAdapter.setData(list);
-            } catch (JSONException e) {
-                e.printStackTrace();
-                Toast.makeText(getBaseContext(), "Data response is empty", Toast.LENGTH_SHORT).show();
-            }
-            mSwipeRefreshLayout.setRefreshing(false);  //turn of the refresh.
+            mkmsg(page);
         }
-
-
     }
+
 }
