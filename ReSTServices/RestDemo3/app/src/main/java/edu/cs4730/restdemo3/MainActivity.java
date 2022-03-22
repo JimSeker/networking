bfec021462f422ab35f2fbc5abee561d9adc7466
Jim Ward <seker@uwyo.edu>
2022-03-22 14:18:51 -0600

    replaced the last asynctask with a thread.

--- ReSTServices/RestDemo3/app/src/main/java/edu/cs4730/restdemo3/MainActivity.java
+++ ReSTServices/RestDemo3/app/src/main/java/edu/cs4730/restdemo3/MainActivity.java
@@ -1,8 +1,5 @@
 package edu.cs4730.restdemo3;
 
-import android.os.AsyncTask;
-import android.os.Bundle;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -15,6 +12,7 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.util.Log;
 import android.view.View;
+import android.os.Bundle;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -84,20 +82,26 @@ public class MainActivity extends AppCompatActivity {
     void doupdate() {
         mSwipeRefreshLayout.setRefreshing(true);
         try {
-            new doNetwork().execute(new URL("http://www.cs.uwyo.edu/~seker/rest/querypic.php"));
+            new Thread(new doNetwork(new URL("http://www.cs.uwyo.edu/~seker/rest/querypic.php"))).start();
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
     }
 
-    /*
-     * Shows how to use an AsyncTask with a httpURLconnection method to query the REST service.
+    /**
+     * Shows how to use a thread with a httpURLconnection method to query the REST service.
+     * Note this was a asynctask, I just convereted to a thread, but keep all the methods.
      */
-    private class doNetwork extends AsyncTask<URL, String, String> {
+    private class doNetwork implements Runnable {
         ArrayList<myObj> list = null;
+        URL myUrl;
+
+        doNetwork(URL url) {
+            myUrl = url;
+        }
 
         //Simple class that takes an InputStream and return the data
-        //as a string, with line sepratorss (ie end of line markers)
+        //as a string, with line separators (ie end of line markers)
         private String readStream(InputStream in) {
             BufferedReader reader = null;
             StringBuilder sb = new StringBuilder("");
@@ -106,7 +110,7 @@ public class MainActivity extends AppCompatActivity {
             try {
                 reader = new BufferedReader(new InputStreamReader(in));
                 while ((line = reader.readLine()) != null) {
-                    publishProgress(line);  //create the data structure as we go.
+                    onProgressUpdate(line);  //create the data structure as we go.
                     sb.append(line).append(NL);
                 }
             } catch (IOException e) {
@@ -124,58 +128,52 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-        @Override
-        protected String doInBackground(URL... params) {
+        public void run() {
             String page = "";
             list = new ArrayList<myObj>();
             try {
                 //setup password authentication
                 Authenticator.setDefault(new myAdapter.MyAuthenticator());
-                //URL url = new URL(params[0].toString()); //but next line is much better! convert directly.
-                URL url = params[0];
-
-                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                HttpURLConnection con = (HttpURLConnection) myUrl.openConnection();
                 page = readStream(con.getInputStream());
                 con.disconnect();
             } catch (Exception e) {
                 // publishProgress("Failed to retrieve web page ...\n");
                 e.printStackTrace();
-                publishProgress(e.getMessage());
 
             }
-            return page;  //return the page downloaded.
+            onPostExecute(page);  //return the page downloaded.
         }
 
-        /*
-         * This takes the place of handlers and my makemsg method, since we can directly access the screen.
-         */
-        protected void onProgressUpdate(String... progress) {
+        protected void onProgressUpdate(String progress) {
             //build the data structure as we go.
-            // output.append(progress[0]);
-            //Log.v("progress", progress[0]);
+
             try {
-                String[] parts = progress[0].split(",");
+                String[] parts = progress.split(",");
                 if (parts.length >= 2) {
                     Log.v("done", parts[0] + " " + parts[1] + " " + parts[2]);
                     list.add(new myObj(Integer.parseInt(parts[0]), parts[1], parts[2]));
                 } else {
-                    Log.wtf(TAG, "split failed, " + progress[0]);
+                    Log.wtf(TAG, "split failed, " + progress);
                 }
             } catch (Exception e) {
-                Log.v("donetwork", "Error line: " + progress[0]);
+                Log.v("donetwork", "Error line: " + progress);
                 e.printStackTrace();
             }
         }
 
-        /*
-         * So the file has been downloaded and in this simple example it is displayed
-         * to the screen.
+        /**
+         * So the file has been downloaded and in this simple example it is displayed to the screen.
          */
         protected void onPostExecute(String result) {
             //data structure is ready.
-            mAdapter.setData(list);
-            mSwipeRefreshLayout.setRefreshing(false);  //turn of the refresh.
-
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mAdapter.setData(list);
+                    mSwipeRefreshLayout.setRefreshing(false);  //turn of the refresh.
+                }
+            });
         }
     }
 
