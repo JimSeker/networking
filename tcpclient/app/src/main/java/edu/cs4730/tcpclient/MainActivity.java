26c6a4014e08cbbbcc036ebcf7b0b5c2580974ad
Jim Ward <seker@uwyo.edu>
2022-03-22 08:49:32 -0600

    clean up and minor fixes.

--- tcpclient/app/src/main/java/edu/cs4730/tcpclient/MainActivity.java
+++ tcpclient/app/src/main/java/edu/cs4730/tcpclient/MainActivity.java
@@ -3,22 +3,127 @@ package edu.cs4730.tcpclient;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
 
 /**
  * Note, https://koz.io/android-m-and-the-war-on-cleartext-traffic/
  * In the AndroidManifest.xml there is < application ... android:usesCleartextTraffic="true" ...
  * Actually this app didn't need it to work, but that is likely to change.
+ * 
+ * this is a simple network client example.  Note it assumes you are using the emulators, but will work
+ * on phones as well.  You just need to know the IP address.
  */
 
+
 public class MainActivity extends AppCompatActivity {
+    TextView logger;
+    Button mkconn;
+    EditText hostname, port;
+    Thread myNet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                .add(R.id.container, new MainFragment()).commit();
+        logger = findViewById(R.id.logger);
+        logger.append("\n");
+        hostname = findViewById(R.id.EThostname);
+        //This address is the localhost for the computer the emulator is running on.  If you are running
+        //tcpserv in another emulator on the same machine, use this address
+        hostname.setText("10.0.2.2");
+        //This would be more running on the another phone or different host and likely not this ip address either.
+        //hostname.setText("10.121.174.200");
+        port = findViewById(R.id.ETport);
+        mkconn = findViewById(R.id.makeconn);
+        mkconn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doNetwork stuff = new doNetwork();
+                myNet = new Thread(stuff);
+                myNet.start();
+            }
+        });
+    }
+
+    private Handler handler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            logger.append(msg.getData().getString("msg"));
+            return true;
+        }
+
+    });
+
+    public void mkmsg(String str) {
+        //handler junk, because thread can't update screen!
+        Message msg = new Message();
+        Bundle b = new Bundle();
+        b.putString("msg", str);
+        msg.setData(b);
+        handler.sendMessage(msg);
+    }
+
+    /**
+     * this code does most of the work in a thread, so that it doesn't lock up the activity_main (UI) thread
+     * It call mkmsg (which calls the handler to update the screen)
+     */
+    class doNetwork implements Runnable {
+        public PrintWriter out;
+        public BufferedReader in;
+
+        public void run() {
+            int p = Integer.parseInt(port.getText().toString());
+            String h = hostname.getText().toString();
+            mkmsg("host is " + h + "\n");
+            mkmsg(" Port is " + p + "\n");
+            try {
+                InetAddress serverAddr = InetAddress.getByName(h);
+                mkmsg("Attempt Connecting..." + h + "\n");
+                Socket socket = new Socket(serverAddr, p);
+                String message = "Hello from Client android emulator";
+
+                //made connection, setup the read (in) and write (out)
+                out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                //now send a message to the server and then read back the response.
+                try {
+                    //write a message to the server
+                    mkmsg("Attempting to send message ...\n");
+                    out.println(message);
+                    mkmsg("Message sent...\n");
+
+                    //read back a message from the server.
+                    mkmsg("Attempting to receive a message ...\n");
+                    String str = in.readLine();
+                    mkmsg("received a message:\n" + str + "\n");
+
+                    mkmsg("We are done, closing connection\n");
+                } catch (Exception e) {
+                    mkmsg("Error happened sending/receiving\n");
+
+                } finally {
+                    in.close();
+                    out.close();
+                    socket.close();
+                }
+
+            } catch (Exception e) {
+                mkmsg("Unable to connect...\n");
+            }
         }
     }
 }
--- tcpserv/app/src/main/java/edu/cs4730/tcpserv/MainActivity.java
+++ tcpserv/app/src/main/java/edu/cs4730/tcpserv/MainActivity.java
@@ -2,7 +2,25 @@ package edu.cs4730.tcpserv;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Service;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.text.format.Formatter;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  * Note, https://koz.io/android-m-and-the-war-on-cleartext-traffic/
@@ -16,20 +34,120 @@ import android.os.Bundle;
  * <p>
  * Note, that while the IP address of the server is listed, if you are running on emulator use the host
  * address in tcpclient ip 10.0.2.2 (this is the address used by android for the host system.
- *
+ * <p>
  * If you want to talk the server from program like telnet, or whatever.  localhost: 3012  is the address
- *
+ * <p>
+ * This a simple network server code.  The user must push the make connection button to setup the
+ * server to accept connections.
+ * <p>
+ * Note use adb forward tcp:3012 tcp:3012
+ * to setup the emulator to receive.
  */
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView logger;
+    Button mkconn;
+    EditText port;
+    Thread myNet;
+
+    String TAG = "TCPserv";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                .add(R.id.container, new MainFragment()).commit();
+        logger = findViewById(R.id.logger);
+        logger.append("\n\n");
+        port = findViewById(R.id.ETport);
+        mkconn = findViewById(R.id.makeconn);
+        mkconn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //this way creates the thread anonymously.  quick and dirty, but generally a bad idea.
+                //new Thread(new doNetwork()).start();
+
+                //better way is this way, where we have access to the thread variable.
+                doNetwork stuff = new doNetwork();
+                myNet = new Thread(stuff);
+                myNet.start();
+            }
+        });
+
+        //What is our IP address?
+        WifiManager wm = (WifiManager) getSystemService(Service.WIFI_SERVICE);
+        //noinspection deprecation    wifi can't return a ipv6, which is what the issue is, formater doesn't support ipv6
+        String ip = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
+        logger.append("Server IP address is " + ip + "\n");
+    }
+
+    /**
+     * simple hepler method to update the logger/output textview.
+     */
+    public void mkmsg(String str) {
+        //so the client uses a handler, while the server code is going to use runUI method.
+        //the method name makes no sense here, but keeping the name, so it matches the tpcclient.
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                logger.append(str);
+            }
+        });
+    }
+
+    /**
+     * Most of the work is done here, so it does not lock the UI thread.  Calls mkmsg, which run on the main thread to do updates.
+     *
+     * protocol for the example is the server reads in a line and then sends a line.
+     * then it closes the connection.
+     */
+    private class doNetwork implements Runnable {
+        public void run() {
+
+            int p = Integer.parseInt(port.getText().toString());
+            mkmsg(" Port is " + p + "\n");
+
+            try {
+                mkmsg("Waiting on Connecting...\n");
+                Log.v(TAG, "S: Connecting...");
+                ServerSocket serverSocket = new ServerSocket(p);
+
+                //socket created, now wait for a coonection via accept.
+                Socket client = serverSocket.accept();
+                Log.v(TAG, "S: Receiving...");
+
+                try {
+                    //setup send/receive streams.
+                    BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                    PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(client.getOutputStream())), true);
+
+                    //receive the message first.
+                    mkmsg("Attempting to receive a message ...\n");
+                    String str = in.readLine();
+                    mkmsg("received a message:\n" + str + "\n");
+
+                    //now send a message.
+                    String message = "Hello from server android emulator";
+                    mkmsg("Attempting to send message ...\n");
+                    out.println(message);
+                    mkmsg("Message sent...\n");
+
+                    //now close down the send/receive streams.
+                    in.close();
+                    out.close();
+
+                } catch (Exception e) {
+                    mkmsg("Error happened sending/receiving\n");
+                } finally {
+                    mkmsg("We are done, closing connection\n");
+                    client.close();  //close the client connection
+                    serverSocket.close();  //finally close down the server side as well.
+                }
+            } catch (Exception e) {
+                mkmsg("Unable to connect...\n");
+                e.printStackTrace();
+            }
         }
     }
 }
