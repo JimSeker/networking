26b37576285783020c1bb11be0a5ac1972195da4
Jim Ward <JimSeker@users.noreply.github.com>
2024-11-12 08:15:48 -0700

    fixed error in android template code.

--- ReSTServices/RestDemo2/app/src/main/java/edu/cs4730/restdemo2/MainActivity.java
+++ ReSTServices/RestDemo2/app/src/main/java/edu/cs4730/restdemo2/MainActivity.java
@@ -78,7 +78,7 @@ public class MainActivity extends AppCompatActivity implements myDialogFragment.
         ViewCompat.setOnApplyWindowInsetsListener(binding.main, (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+            return WindowInsetsCompat.CONSUMED;
         });
         setSupportActionBar(binding.toolbar);
 
